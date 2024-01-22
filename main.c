4f075fba82cec812dac5a5d3ae07cce4d8669a4c
myGitDemo1 <132230594+myGitDemo1@users.noreply.github.com>
2024-01-22 22:09:46 +0400

    Update new version C code
     32 Times Hello School

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf("Hello School\n"); // Change the output message
   }
   return 0;
 }
